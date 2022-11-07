a20863aa16e67aab4fdcf8bc81cc3c88514e1783
thor <thorck@protonmail.com>
2022-11-06 17:35:17 -0800

    uh guess i gotta start an agent

--- files.sh
+++ files.sh
@@ -6,7 +6,7 @@ echo "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> /home/thor/cron
 
 cd /home/thor/.files
 # Need to start an ssh agent to be able to push to github
-# eval $(ssh-agent) 
+eval $(ssh-agent) 
 echo syntaxoverl0rd | ssh-add /home/thor/.ssh/id_ed25519
 
 # This will throw errors for oh-my-zsh, this is fine
--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 echo "cronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)" >> /home/thor/cron/logs/setup
 
 cd /home/thor/.setup
-# eval $(ssh-agent) 
+eval $(ssh-agent) 
 echo syntaxoverl0rd | ssh-add /home/thor/.ssh/id_ed25519
 
 git add --all 
