50d06481344a8808f1c842ea16ec05b9b754b398
Thor Kamphefner <thorck@pm.me>
2023-09-12 14:46:07 -0700

    debug sync dirs

--- git_sync_directories.sh
+++ git_sync_directories.sh
@@ -5,11 +5,11 @@
 # List of directories to process
 set directories /home/thor/.files /home/thor/.setup /home/thor/.cron /home/thor/.private /home/thor/r/tmpl /home/thor/.keep /home/thor/img/backgrounds /home/thor/img/profile /home/thor/blog
 
-echo -e "\ncronlog: (hostname)-(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> /home/thor/.cron/logs/sync_directories
+echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> /home/thor/.cron/logs/sync_directories
 
 # Need to start an ssh agent to be able to push to GitHub
 if test -z "$SSH_AUTH_SOCK"
-    eval (ssh-agent)
+    eval $(ssh-agent)
     ssh-add -k /home/thor/.ssh/id_ed25519_cron
 end
 
@@ -20,12 +20,12 @@ for dir in $directories
 
     # This will throw errors for oh-my-zsh, this is fine
     git submodule foreach git add --all
-    git submodule foreach git commit -m "(hostname)-(date -u +%Y-%m-%d\ %H:%M%Z)"
+    git submodule foreach git commit -m "$(hostname)-(date -u +%Y-%m-%d\ %H:%M%Z)"
     git submodule foreach git pull
     git submodule foreach git push
 
     git add --all
-    git commit -m "(hostname)-(date -u +%Y-%m-%d\ %H:%M%Z)"
+    git commit -m "$(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)"
     git pull
     git push
 
