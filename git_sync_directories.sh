f18df17551f89459437f2663b955c35538b4d31a
Thor Kamphefner <thorck@pm.me>
2023-09-12 14:10:01 -0700

    boorb-2023-09-12 21:10UTC

---
+++ git_sync_directories.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env fish
+# Back up dotfiles and run dotbot for multiple directories
+# Set this up in cron to run every 10 minutes
+
+# List of directories to process
+set directories "/home/thor/.files /home/thor/.setup /home/thor/.cron /home/thor/.private /home/thor/r/tmpl"
+
+echo -e "\ncronlog: (hostname)-(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> /home/thor/.cron/logs/sync_directories
+
+# Need to start an ssh agent to be able to push to GitHub
+if test -z "$SSH_AUTH_SOCK"
+    eval (ssh-agent)
+    ssh-add -k /home/thor/.ssh/id_ed25519_cron
+end
+
+# Loop through each directory and perform operations
+for dir in $directories
+    cd $dir
+
+    # This will throw errors for oh-my-zsh, this is fine
+    git submodule foreach git add --all
+    git submodule foreach git commit -m "(hostname)-(date -u +%Y-%m-%d\ %H:%M%Z)"
+    git submodule foreach git pull
+    git submodule foreach git push
+
+    git add --all
+    git commit -m "(hostname)-(date -u +%Y-%m-%d\ %H:%M%Z)"
+    git pull
+    git push
+
+end
+
+# Kill the ssh-agent
+ssh-agent -k
+set -e SSH_AUTH_SOCK
+set -e SSH_AGENT_PID
+
+/home/thor/.local/bin/dotbot -c /home/thor/.files/install.conf.yaml
+/home/thor/.local/bin/dotbot -c /home/thor/.private/install.conf.yaml
+
