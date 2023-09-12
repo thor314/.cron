5e28249788baf0dcb08425330b91b5f576ef2c7e
Thor Kamphefner <thorck@pm.me>
2023-09-12 15:11:43 -0700

    clean home

---
+++ clean_home_env.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+# remove crap from my home 
+
+echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> /home/thor/.cron/logs/clean_home.log
+
+set files_to_remove ~/.bash_history ~/.lesshst ~/.viminfo ~/.wget-hsts ~/.gp_history ~/.selected_editor ~/.sudo_as_admin_succesful
+set dirs_to_remove ~/.dotnet ~/.trash
+
+for f in $files_to_remove
+  if test -e $f
+    echo "Removing file: $f"
+    rm $f
+  end
+end
+
+for d in $dirs_to_remove
+  if test -d $d
+    echo "Removing file: $d"
+    rm $d -r
+  end
+end
+
--- sync_updates.sh
+++ sync_updates.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env fish
 # commands run on one machine that should be run on all machines to keep everyone in-sync
 
+echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> /home/thor/.cron/logs/sync_updates.log
+
