dd222cd50c6da85a8f6d81f72c63a7147ec1f14e
Thor Kamphefner <thorck@pm.me>
2023-09-14 13:35:08 -0700

    boorb-2023-09-14 20:35UTC

--- sync_updates.sh
+++ sync_updates.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env fish
+#!/usr/bin/fish
 # commands run on one machine that should be run on all machines to keep everyone in-sync
 
 echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> /home/thor/.cron/logs/sync_updates.log
