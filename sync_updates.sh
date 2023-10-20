91315007587c4a9f28a57c42e181928617a3071c
Thor Kamphefner <thorck@pm.me>
2023-10-20 11:25:07 +0100

    pop-os-2023-10-20 10:25UTC

--- sync_updates.sh
+++ sync_updates.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/fish
 # commands run on one machine that should be run on all machines to keep everyone in-sync
 
+# disable noise errors that X display cannot be opened
+fish set -x DISPLAY :0
+
+rm /home/thor/.cron/logs/sync_updates.log
 echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> /home/thor/.cron/logs/sync_updates.log
 
