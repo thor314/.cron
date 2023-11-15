bcd290b69f26ac255131d514109c4d0889abde80
Thor Kamphefner <thorck@pm.me>
2023-11-15 12:59:58 +0300

    clean home log adjustment

--- clean_home.sh
+++ clean_home.sh
@@ -2,6 +2,7 @@
 # remove crap from my home 
 
 set LOGFILE '/home/thor/.cron/logs/clean_home.log'
+tail -n 100 $LOGFILE > $LOGFILE
 rm $LOGFILE 
 
 echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> $LOGFILE
