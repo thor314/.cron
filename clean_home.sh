c6fc90e06989eccfd48cf73461ebc6ede56923e1
Thor Kamphefner <thorck@pm.me>
2023-11-15 12:50:16 +0300

    pop-os-2023-11-15 09:50UTC

--- clean_home.sh
+++ clean_home.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env fish
 # remove crap from my home 
 
-echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> /home/thor/.cron/logs/clean_home.log
+set LOGFILE '/home/thor/.cron/logs/clean_home.log'
+echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> $LOGFILE
+
+# disable noise errors that X display cannot be opened
+fish set -x DISPLAY :0
 
 set files_to_remove ~/.bash_history ~/.lesshst ~/.viminfo ~/.wget-hsts ~/.gp_history ~/.selected_editor ~/.sudo_as_admin_successful ~/.bash_logout ~/.bashrc ~/.python_history ~/.tsconfig.json 
 
@@ -9,14 +13,14 @@ set dirs_to_remove ~/.dotnet ~/.trash ~/.1password
 
 for f in $files_to_remove
     if test -e $f
-        echo -e "Removing file: $f" >> /home/thor/.cron/logs/clean_home.log
+        echo -e "Removing file: $f" >> $LOGFILE
         rm $f
     end
 end
 
 for d in $dirs_to_remove
     if test -d $d
-        echo -e "Removing dir: $d" >> /home/thor/.cron/logs/clean_home.log
+        echo -e "Removing dir: $d" >> $LOGFILE
         rm $d -r
     end
 end
