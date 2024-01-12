c6098a20e483f37ce63715eadd58f355cbc6fda9
Thor Kamphefner <thorck@pm.me>
2024-01-12 14:22:20 -0800

    birb-2024-01-12 22:22UTC

--- git_sync_directories.sh
+++ git_sync_directories.sh
@@ -5,12 +5,10 @@
 # List of directories to process
 set LOGFILE $HOME/.cron/logs/sync_dirs.log
 set dirs $HOME/.files $HOME/.setup $HOME/.cron $HOME/.private $HOME/.keep 
-set dirs $dirs $HOME/img/backgrounds $HOME/img/profile $HOME/img/official-images 
-set dirs $dirs $HOME/blog $HOME/obsidian-setup
-# to remove:
-set dirs $dirs $HOME/invoice $HOME/official 
+set dirs $dirs $HOME/projects 
+echo $dirs
 
-rm $LOGFILE
+rm $LOGFILE # rotate the log file, could be more sophisticated, but..meh
 
 echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> $LOGFILE 
 
@@ -19,30 +17,35 @@ fish set -x DISPLAY :0
 
 # fish shell-specific:
 eval (ssh-agent -c) >> $LOGFILE
-ssh-add /home/thor/.ssh/id_ed25519_cron >> $LOGFILE ^&1
+ssh-add /home/thor/.ssh/id_ed25519_cron >> $LOGFILE 
+# ssh-add /home/thor/.ssh/id_ed25519_cron >> $LOGFILE 2>&1
 
 # bash shell equivalent: 
 # eval $(ssh-agent) >> /home/thor/log
 # ssh-add /home/thor/.ssh/id_ed25519_cron >> /home/thor/log 2>&1
 
+echo "1" && sleep 5
+
 # Loop through each directory and perform operations
 for dir in $dirs
     cd $dir 
-    echo -e "\n visiting $dir \n" >> $LOGFILE
+    echo -e "\n visiting $dir \n" 
 
     # This will throw errors for oh-my-zsh, this is fine
-    git submodule foreach git add --all
-    git submodule foreach git commit -m $(hostname)-(date -u +%Y-%m-%d\ %H:%M%Z)
+    git submodule foreach git add --all 
+    git submodule foreach git commit -m $(hostname)-(date -u +%Y-%m-%d\ %H:%M%Z) 
     git submodule foreach git pull
     git submodule foreach git push 
 
-    git add --all
-    git commit -m $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)
-    git pull
-    git push && notify-send "Success" "successful"
+    git add --all 
+    git commit -m $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z) 
+    git pull 
+    git push && notify-send "Success" "successfully updated $dir" 
 
-    echo -e "\n leaving $dir \n" >> $LOGFILE
-end
+    echo -e "\n leaving $dir \n" 
+end &>> $LOGFILE 
+
+echo "2" && sleep 5
 
 # Kill the ssh-agent, don't leak resources
 ssh-agent -k >> $LOGFILE
