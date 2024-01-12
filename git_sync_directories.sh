cdfa448f3d25fc4079d92b2b55dd596a5dc926bb
Thor Kamphefner <thorck@pm.me>
2024-01-12 15:15:57 -0800

    birb-2024-01-12 23:15UTC

--- git_sync_directories.sh
+++ git_sync_directories.sh
@@ -4,52 +4,65 @@
 
 # List of directories to process
 set LOGFILE $HOME/.cron/logs/sync_dirs.log
-set dirs $HOME/.files $HOME/.setup $HOME/.cron $HOME/.private $HOME/.keep 
-set dirs $dirs $HOME/projects 
-echo $dirs
+set dirs $HOME/.setup $HOME/.cron $HOME/.private $HOME/.keep 
+set dirs $dirs $HOME/projects # contains submodules
+set dirs $dirs $HOME/.files # contains submodules
+set COMMIT_MSG "$(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)"
 
-rm $LOGFILE # rotate the log file, could be more sophisticated, but..meh
+# rotate the log file, could be more sophisticated, but..meh
+rm $LOGFILE 
 
-echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" >> $LOGFILE 
+echo ============================ &>> $LOGFILE
+echo -e "cronlog: $COMMIT_MSG"    &>> $LOGFILE 
+echo "updating $dirs..."          &>> $LOGFILE
+echo ============================ &>> $LOGFILE
 
 # disable noise errors that X display cannot be opened
-fish set -x DISPLAY :0
+fish set -x DISPLAY :0 &>> $LOGFILE
 
 # fish shell-specific:
-eval (ssh-agent -c) >> $LOGFILE
-ssh-add /home/thor/.ssh/id_ed25519_cron >> $LOGFILE 
-# ssh-add /home/thor/.ssh/id_ed25519_cron >> $LOGFILE 2>&1
-
-# bash shell equivalent: 
+eval (ssh-agent -c) &>> $LOGFILE
+ssh-add /home/thor/.ssh/id_ed25519_cron &>> $LOGFILE 
+# In bash, this is equivalent to (don't uncomment or remove)
 # eval $(ssh-agent) >> /home/thor/log
 # ssh-add /home/thor/.ssh/id_ed25519_cron >> /home/thor/log 2>&1
-
-echo "1" && sleep 5
 
 # Loop through each directory and perform operations
 for dir in $dirs
     cd $dir 
-    echo -e "\n visiting $dir \n" 
+    echo "--------------------------------"
+    echo -e "visiting $dir" 
 
-    # This will throw errors for oh-my-zsh, this is fine
-    git submodule foreach git add --all 
-    git submodule foreach git commit -m $(hostname)-(date -u +%Y-%m-%d\ %H:%M%Z) 
-    git submodule foreach git pull
-    git submodule foreach git push 
+    if test -f .gitmodules
+      echo "********************************"
+      echo "updating $dir submodules" 
+      git submodule foreach "
+        echo -e \"visiting $dir\" 
+        git add . && 
+        git diff --cached --exit-code --quiet || git commit -m \"$COMMIT_MSG\"; 
+        git pull && 
+        git push
+        echo \"--------------------------------\"
+        "
+      echo "updated $dir submodules" 
+      echo "********************************"
+    end
 
+    echo  "updating $dir" 
     git add --all 
-    git commit -m $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z) 
+    git commit -m $COMMIT_MSG
     git pull 
     git push && notify-send "Success" "successfully updated $dir" 
 
-    echo -e "\n leaving $dir \n" 
+    echo -e "leaving $dir " 
+    echo "--------------------------------"
 end &>> $LOGFILE 
 
-echo "2" && sleep 5
-
+echo -e "Finished syncing" 
+echo "==================================="
 # Kill the ssh-agent, don't leak resources
-ssh-agent -k >> $LOGFILE
+ssh-agent -k &>> $LOGFILE
 
-/home/thor/.local/bin/dotbot -c /home/thor/.files/install.conf.yaml
-/home/thor/.local/bin/dotbot -c /home/thor/.private/install.conf.yaml
+/home/thor/.local/bin/dotbot -c /home/thor/.files/install.conf.yaml &>> $LOGFILE
+/home/thor/.local/bin/dotbot -c /home/thor/.private/install.conf.yaml &>> $LOGFILE
 
