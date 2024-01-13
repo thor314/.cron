e26406225ea0477482b1d3a754948dd3c1eb41c2
Thor Kamphefner <thorck@pm.me>
2024-01-13 00:20:07 -0800

    "boorb-2024-01-13 08:20UTC"

--- clean_home.sh
+++ clean_home.sh
@@ -3,25 +3,32 @@
 
 set LOGFILE ~/.cron/logs/clean_home.log
 fish ~/.cron/help_scripts/rotate_logs.sh $LOGFILE
-
-# disable noise errors that X display cannot be opened
-set -x DISPLAY :0 &>> $LOGFILE
 set files ~/.bash_history ~/.lesshst ~/.viminfo ~/.wget-hsts ~/.gp_history ~/.selected_editor ~/.sudo_as_admin_successful ~/.bash_logout ~/.bashrc ~/.python_history ~/.tsconfig.json 
 set dirs ~/.dotnet ~/.trash ~/.1password ~/.java ~/.gap .jupyter .vscode-insiders ~/.fltk ~/.jmol
 
-echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" &>> $LOGFILE
+function clean_home
+  # disable noise errors that X display cannot be opened
+  set -x DISPLAY :0 
 
-for f in $files
-    if test -f $f
-        echo "Removing file: $f" 
-        rm $f
-    end
-end &>> $LOGFILE
+  echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  echo -e "\ncronlog: $(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)\n" 
+  echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-for d in $dirs
-    if test -d $d
-        echo "Removing dir: $d" 
-        rm $d -r
-    end
-end &>> $LOGFILE 
+  for f in $files
+      if test -f $f
+          echo "Removing file: $f" 
+          rm $f
+      end
+  end
 
+  echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  for d in $dirs
+      if test -d $d
+          echo "Removing dir: $d" 
+          rm $d -r
+      end
+  end
+end
+
+clean_home &>> $LOGFILE
