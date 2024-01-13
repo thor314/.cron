c8009c95abe7036bc7b95dd1ad00e6c3e83b803a
Thor Kamphefner <thorck@pm.me>
2024-01-12 23:40:14 -0800

    "boorb-2024-01-13 07:40UTC"

--- git_sync_directories.sh
+++ git_sync_directories.sh
@@ -4,10 +4,7 @@
 # Failed to execute child process “dbus-launch” (No such file or directory)
 
 set LOGFILE $HOME/.cron/logs/sync_dirs.log
-fish ~/.cron/help_scripts/rotate_logs.sh
-
-# disable noisy errors that X display cannot be opened
-set -x DISPLAY :0 &>> $LOGFILE
+fish ~/.cron/help_scripts/rotate_logs.sh $LOGFILE
 set COMMIT_MSG "$(hostname)-$(date -u +%Y-%m-%d\ %H:%M%Z)"
 
 # List of directories to process
@@ -16,48 +13,66 @@ set dirs $HOME/.setup $HOME/.cron $HOME/.private $HOME/.keep
 set dirs $dirs $HOME/projects 
 set dirs $dirs $HOME/.files 
 
-echo ============================ &>> $LOGFILE
-echo -e "cronlog: $COMMIT_MSG"    &>> $LOGFILE 
-echo "updating $dirs..."          &>> $LOGFILE
-echo ============================ &>> $LOGFILE
+function update_dirs
+    set dirs $argv
+    # disable noisy errors that X display cannot be opened
+    set -x DISPLAY :0 
 
-# fish shell-specific:
-eval (ssh-agent -c) &>> $LOGFILE
-ssh-add $HOME/.ssh/id_ed25519_cron &>> $LOGFILE 
-# In bash, this is equivalent to (don't uncomment or remove)
-# eval $(ssh-agent) >> /home/thor/log
-# ssh-add /home/thor/.ssh/id_ed25519_cron >> /home/thor/log 2>&1
+    echo ============================ 
+    echo -e "cronlog: $COMMIT_MSG"    
+    echo "updating $dirs..."          
+    echo ============================ 
 
-# Loop through each directory and perform operations
-for dir in $dirs
-    cd $dir 
-    echo "--------------------------------"
-    echo -e "visiting $dir" 
+    # fish shell-specific:
+    eval (ssh-agent -c)
+    ssh-add $HOME/.ssh/id_ed25519_cron 
+    # In bash, this is equivalent to (don't uncomment or remove)
+    # eval $(ssh-agent) >> /home/thor/log
+    # ssh-add /home/thor/.ssh/id_ed25519_cron >> /home/thor/log 2>&1
 
-    if test -f .gitmodules
-      echo "********************************"
-      echo "updating $dir submodules" 
-      # need the git diff line, or submodule will exit early
-      git submodule foreach "
-        echo -e \"visiting $dir\" 
-        git add . 
-        git diff --cached --exit-code --quiet || git commit -m \"$COMMIT_MSG\"
-        git pull && git push && notify-send "Submodule updated" "successfully updated $dir" 
-        echo \"--------------------------------\"
-        "
-      echo "updated $dir submodules" 
-      echo "********************************"
-    end
+    # Loop through each directory and perform operations
+    for dir in $dirs
+        cd $dir 
+        echo "--------------------------------"
+        echo -e "visiting $dir" 
 
+        if test -f .gitmodules
+          update_submodules $dir
+        end
+
+        update_dir $dir
+        echo "--------------------------------"
+    end 
+
+    echo -e "Finished syncing" 
+    echo "===================================" 
+    # Kill the ssh-agent, don't leak resources
+    ssh-agent -k 
+end
+
+function update_submodules
+    set dir $argv[1]
+    echo "********************************"
+    echo "updating $dir submodules" 
+    # need the git diff line, or submodule will exit early
+    git submodule foreach "
+      echo -e \"visiting $dir\" 
+      git add . 
+      git diff --cached --exit-code --quiet || git commit -m \"$COMMIT_MSG\"
+      git pull && git push && notify-send \"Submodule updated\" \"successfully updated $dir\" 
+      echo \"--------------------------------\"
+      "
+    echo "updated $dir submodules" 
+    echo "********************************"
+end
+
+function update_dir
+    set dir $argv[1]
     echo "updating $dir" 
     git add --all 
     git diff --cached --exit-code --quiet || git commit -m \"$COMMIT_MSG\"
     git pull && git push && notify-send "Directory updated" "successfully updated $dir" 
-    echo -e "leaving $dir " 
-    echo "--------------------------------"
-end &>> $LOGFILE 
+    echo "leaving $dir " 
+end
 
-echo -e "Finished syncing" &>> $LOGFILE
-echo "===================================" &>> $LOGFILE
-# Kill the ssh-agent, don't leak resources
-ssh-agent -k &>> $LOGFILE
+update_dirs $dirs &>> $LOGFILE
