8cc64a52d0fd4a96dc762f5a1e2b904fdc714a02
Thor Kamphefner <thorck@pm.me>
2024-01-12 20:20:04 -0800

    "boorb-2024-01-13 04:20UTC"

---
+++ auto_publish.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/fish
+# Maintain the list of resources I want auto-published every day out of my Obsidian vault
+# TODO
+
+# dictionary: file:heading_name in my obsidian vault -> file:heading name elsewhere
+
+# move images
+
+function copy_file 
+
+end
