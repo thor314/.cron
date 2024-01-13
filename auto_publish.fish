acd3a9a2cf893aceefc140c393089dfb4d897d08
Thor Kamphefner <thorck@pm.me>
2024-01-12 22:30:05 -0800

    "boorb-2024-01-13 06:30UTC"

--- auto_publish.fish
+++ auto_publish.fish
@@ -12,7 +12,7 @@ set TMP_FILE "/tmp/cron-publish-$DATE"
 function copy_file 
     set SOURCE_FILE $argv[1]
     set DEST_FILE $argv[2]
-    set SOURCE_HEADING $argv[3]
+    set HEADING $argv[3]
 
     if not test -f $SOURCE_FILE
         echo "Source file does not exist"
@@ -22,43 +22,52 @@ function copy_file
         echo "Destination file does not exist"
         return 1
     end
-    if not rg -q "^$SOURCE_HEADING\$" $SOURCE_FILE
-        echo "Source file does not contain the heading $SOURCE_HEADING"
+    if not rg -q "^$HEADING\$" $SOURCE_FILE
+        echo "Source file does not contain the heading $HEADING"
         return 1
     end
-    if not rg -q "^$SOURCE_HEADING\$" $DEST_FILE
-        echo "Destination file does not contain the heading $SOURCE_HEADING"
+    if not rg -q "^$HEADING\$" $DEST_FILE
+        echo "Destination file does not contain the heading $HEADING"
         return 1
     end
 
     # extract the section to a tmp file
-    set LINE_NUMBERS (get_line_numbers $SOURCE_FILE $SOURCE_HEADING)
+    set LINE_NUMBERS (get_line_numbers $SOURCE_FILE $HEADING)
     set START_LINE (echo $LINE_NUMBERS | cut --delimiter=" " -f1)
     set END_LINE (echo $LINE_NUMBERS | cut --delimiter=" " -f2)
     set N_TOTAL_LINES (math "$END_LINE - $START_LINE + 1")
     echo "cutting from line $START_LINE to $END_LINE in $SOURCE_FILE"
     head $SOURCE_FILE -n $END_LINE | tail -n $N_TOTAL_LINES > $TMP_FILE
-
     echo we captured:
     cat $TMP_FILE
 
     # todo: If I eventually get less lazy, maybe handle moving images too 
 
+    # Replace the section in the destination file
+    set LINE_NUMBERS (get_line_numbers $DEST_FILE $HEADING)
+    set START_LINE (echo $LINE_NUMBERS | cut --delimiter=" " -f1)
+    set END_LINE (echo $LINE_NUMBERS | cut --delimiter=" " -f2)
+    set N_TOTAL_LINES (math "$END_LINE - $START_LINE + 1")
+    echo "cutting from line $START_LINE to $END_LINE in $DEST_FILE"
+    head $DEST_FILE -n $END_LINE | tail -n $N_TOTAL_LINES > $TMP_FILE.old
+
+    # if /home/thor/.cargo/bin/difft $TMP_FILE $TMP_FILE.old >> /dev/null
+    #     echo changes detected
+    #     # Replace the lines in $DEST_FILE between $START_LINE and $END_LINE with the lines in $TMP_FILE
+
+    # end
+    if /home/thor/.cargo/bin/difft $TMP_FILE $TMP_FILE.old >> /dev/null
+        echo changes detected
+        # Replace the lines in $DEST_FILE between $START_LINE and $END_LINE with the lines in $TMP_FILE
+        head -n (math $START_LINE - 1) $DEST_FILE > $TMP_FILE.new
+        cat $TMP_FILE >> $TMP_FILE.new
+        tail -n (math (count (cat $DEST_FILE)) - $END_LINE) $DEST_FILE >> $TMP_FILE
+
+        cp $TMP_FILE $DEST_FILE
+        rm $TMP_FILE.new
+    end
     
-    # # Replace the section in the destination file
-    # # This is a bit tricky and might need a more complex solution like a temporary file
-    # set temp_file (mktemp)
-    # awk -v heading="$SOURCE_HEADING" -v replacement="$section" '
-    #     BEGIN {skip=0}
-    #     /^$/ {skip=0}
-    #     skip {next}
-    #     {print}
-    #     /^'"$SOURCE_HEADING"'$/ {
-    #         print replacement
-    #         skip=1
-    #     }
-    # ' $DEST_FILE > $temp_file
-    # mv $temp_file $DEST_FILE
+    rm $TMP_FILE $TMP_FILE.old 
 end
 
 function get_line_numbers
