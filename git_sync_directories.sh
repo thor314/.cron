1e39ebc7c9ab92985496831096c55b7458c33e16
Thor Kampefner <thorck@pm.me>
2024-01-26 13:10:16 -0800

    "boorb-2024-01-26-21:10UTC"

--- git_sync_directories.sh
+++ git_sync_directories.sh
@@ -8,46 +8,50 @@ set COMMIT_MSG $(hostname)-$(date -u +%Y-%m-%d-%H:%M%Z)
 
 # List of directories to process
 set DIRS $HOME/.setup $HOME/.cron $HOME/.private $HOME/.keep 
-# these dirs contain submodules
-set DIRS $DIRS $HOME/projects $HOME/.files 
+set DIRS $DIRS $HOME/.files 
+# do not create noisy sync commits in projects, do this manually
+set DIRS_NOCOMMIT $HOME/projects 
 
 function update-dirs
-    set dirs $argv
+    set dirs $argv[1..-2]
+    set nocommit $argv[-1]
     # disable noisy errors that X display cannot be opened
     set -x DISPLAY :0 
-
-    echo ============================ 
-    echo -e "cronlog: $COMMIT_MSG"    
-    echo "updating $dirs..."          
-    echo ============================ 
 
     # start an ssh agent. Avoid change to this section. Debugging ssh key permissions is annoying.
     eval (keychain --eval -Q) # output keychain ssh-agent shell info into this script and source it
     keychain --nogui ~/.ssh/id_ed25519 -Q -q # make sure the cron key is added
     echo known ssh keys: (keychain -L) # this should output some keys. If not, we're borked.
 
+    echo ============================ 
+    echo -e "cronlog: $COMMIT_MSG"    
+    echo "updating $dirs..."          
+    echo ============================ 
+
     # Loop through each directory and perform operations
-    for dir in $dirs ; update-dir $dir ; end 
+    for dir in $dirs ; update-dir $dir $nocommit ; end 
 
     echo -e "Finished syncing" 
     echo "===================================" 
-    # 2024-01-25 keychain update - this should no longer be necessary
-    # Kill the ssh-agent, don't leak resources
-    # ssh-agent -k 
 end
 
 function update-dir 
     set dir $argv[1] 
+    set nocommit $argv[2]
     cd $dir 
     echo "--------------------------------"
     echo -e "visiting $dir" 
 
-    if test -f .gitmodules; update-submodules $dir ; end
+    if test -f .gitmodules; update-submodules $dir $nocommit ; end
+
     echo "updating $dir" 
-    git add --all 
-    git diff --cached --exit-code --quiet || git commit -m \"$COMMIT_MSG\"
+    if test $nocommit -eq 1  
+      echo "nocommit"
+    else
+      git add --all 
+      git diff --cached --exit-code --quiet || git commit -m \"$COMMIT_MSG\"
+    end
     git pull && git push 
-    # notify-send "Directory updated" "successfully updated $dir" # mid
     echo "leaving $dir " 
 
     echo "--------------------------------"
@@ -55,20 +59,32 @@ end
 
 function update-submodules
     set dir $argv[1]
+    set nocommit $argv[2]
     echo "********************************"
     echo "updating $dir submodules" 
     # need the git diff line, or submodule will exit early
-    git submodule foreach "
-      echo \"visiting $dir\" 
-      git add . 
-      git checkout main
-      git diff --cached --exit-code --quiet || git commit -m \"$COMMIT_MSG\"
-      git pull && git push 
-      echo \"--------------------------------\"
+    if test $nocommit -eq 1  
+      git submodule foreach "
+        echo \"visiting $dir\" 
+        echo \"nocommit\"
+        git checkout main
+        git pull && git push 
+        echo \"--------------------------------\"
       "
-      # && notify-send \"Submodule updated\" \"successfully updated $dir\" 
+    else
+      git submodule foreach "
+        echo \"visiting $dir\" 
+        git add . 
+        git checkout main
+        git diff --cached --exit-code --quiet || git commit -m \"$COMMIT_MSG\"
+        git pull && git push 
+        echo \"--------------------------------\"
+      "
+    end
+
     echo "updated $dir submodules" 
     echo "********************************"
 end
 
-update-dirs $DIRS &>> $LOGFILE
+update-dirs $DIRS          0 &>> $LOGFILE
+update-dirs $DIRS_NOCOMMIT 1 &>> $LOGFILE
