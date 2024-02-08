c8b48f1aa5a2fdd628be075dd52d16827860a50f
Thor Kampefner <thorck@pm.me>
2024-02-07 16:14:36 -0800

    bump

--- sync_updates.fish
+++ sync_updates.fish
@@ -6,9 +6,12 @@ set LOGFILE /home/thor/.cron/logs/sync_updates.log
 # disable noise errors that X display cannot be opened
 function sync 
   fish ~/.cron/help_scripts/cron_init.fish $LOGFILE
-  /home/thor/.local/bin/dotbot -c /home/thor/.files/install.conf.yaml 
-  /home/thor/.local/bin/dotbot -c /home/thor/.private/install.conf.yaml 
+
   echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  for d in $HOME/{.files,.private}/install.conf.yaml 
+    /home/thor/.local/bin/dotbot -c $d
+    echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  end
 
   # fish $HOME/.cron/help_scripts/sync.fish 
 end 
