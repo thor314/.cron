5b490dd191cf9b0dc5fcfe5c0c5d926bad6144a2
Thor Kampefner <thorck@pm.me>
2024-02-10 12:38:04 -0800

    "boorb-2024-02-10-20:38UTC"

--- apt.fish
+++ apt.fish
@@ -1,13 +1,13 @@
 #!/usr/bin/env fish
-# apt update
+# apt update. must be run with sudo.
 
-set LOGFILE ~/.cron/logs/clean_home.log
+set LOGFILE ~/.cron/logs/apt.log
 
 function tk-apt-update
   fish ~/.cron/help_scripts/cron_init.fish $LOGFILE
   # apt-get not apt. apt will warn about non-stable cli interface.
   DEBIAN_FRONTEND=noninteractive apt-get update 
-  apt-get -y upgrade >> /home/thor/.cron/logs/apt.log 2>&1
+  apt-get -y upgrade 
 end
 
 tk-apt-update &>> $LOGFILE
