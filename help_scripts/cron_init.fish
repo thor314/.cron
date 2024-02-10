baf6ca08c13807a42b26104646039109cf1b55f0
Thor Kampefner <thorck@pm.me>
2024-02-10 12:21:04 -0800

    "boorb-2024-02-10-20:21UTC"

--- help_scripts/cron_init.fish
+++ help_scripts/cron_init.fish
@@ -4,14 +4,20 @@ set LOGFILE $argv[1]
 # rotate the logs
 if test -f $LOGFILE 
   fish ~/.cron/help_scripts/rotate_logs.fish $LOGFILE
-else; echo "ERROR: no log file provided" && exit 1 ; end
+else; echo "ERROR: no log file provided or does not exist" && exit 1 ; end
 # disable noisy errors that X display cannot be opened
-set -gx DISPLAY :0 
-# ensure keychain is running
-source $HOME/.files/fish/functions.fish 
-tk-keychain ~/.ssh/id_ed25519 || echo "ERROR: failed to set up keychain" && exit 1
-# write a pretty log message
-set -gx COMMIT_MSG $(hostname)-$(date -u +%Y-%m-%d-%H:%M%Z)
-echo ============================
-echo -e "cronlog: $COMMIT_MSG"
-echo ============================
+function init
+  set -gx DISPLAY :0 
+  # ensure keychain is running
+  source $HOME/.files/fish/functions.fish 
+  if not tk-keychain ~/.ssh/id_ed25519
+    echo "ERROR: failed to set up keychain" && exit 1
+  end
+  # write a pretty log message
+  set -gx COMMIT_MSG $(hostname)-$(date -u +%Y-%m-%d-%H:%M%Z)
+  echo ============================
+  echo -e "cronlog: $COMMIT_MSG"
+  echo ============================
+end
+init &>> $LOGFILE
+
