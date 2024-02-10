b2d5ec7d16e1863803a0f2ab9a5cef461299a8f4
Thor Kampefner <thorck@pm.me>
2024-02-10 12:23:03 -0800

    "boorb-2024-02-10-20:23UTC"

--- help_scripts/cron_init.fish
+++ help_scripts/cron_init.fish
@@ -6,10 +6,10 @@ if test -f $LOGFILE
   fish ~/.cron/help_scripts/rotate_logs.fish $LOGFILE
 else
   echo "WARNING: creating new logfile" >> $LOGFILE 
-  exit 1 
 end
-# disable noisy errors that X display cannot be opened
+
 function init
+  # disable noisy errors that X display cannot be opened
   set -gx DISPLAY :0 
   # ensure keychain is running
   source $HOME/.files/fish/functions.fish 
@@ -17,9 +17,9 @@ function init
     echo "ERROR: failed to set up keychain" && exit 1
   end
   # write a pretty log message
-  set -gx COMMIT_MSG $(hostname)-$(date -u +%Y-%m-%d-%H:%M%Z)
+  set -gx COMMIT_MSG (hostname)-(date -u +%Y-%m-%d-%H:%M%Z)
   echo ============================
-  echo -e "cronlog: $COMMIT_MSG"
+  echo -e "cronlog: $COMMIT_MSG\n"
   echo ============================
 end
 init &>> $LOGFILE
