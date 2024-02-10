220e6d2977c0cf66b1f225a7c9644af75659f055
Thor Kampefner <thorck@pm.me>
2024-02-10 12:37:03 -0800

    "boorb-2024-02-10-20:37UTC"

--- apt.fish
+++ apt.fish
@@ -1,25 +1,15 @@
 #!/usr/bin/env fish
-# remove crap from my home 
+# apt update
 
 set LOGFILE ~/.cron/logs/clean_home.log
-set FILES ~/{.bash_history, .lesshst, .viminfo, .wget-hsts, .gp_history, .selected_editor, .sudo_as_admin_successful, .bash_logout, .bashrc, .python_history, .tsconfig.json}
-set DIRS ~/{.dotnet, .trash, .1password, .java, .gap, .jupyter, .vscode-insiders, .fltk, .jmol}
 
-function clean_home
+function tk-apt-update
   fish ~/.cron/help_scripts/cron_init.fish $LOGFILE
-
-  for f in $FILES
-      if test -f $f
-          echo "INFO: Removing file: $f" 
-          rm $f || echo "WARNING: could not remove $f"
-      end
-  end
-  for d in $DIRS
-      if test -d $d
-          echo "INFO: Removing dir: $d" 
-          rm $d -r || echo "WARNING: could not remove $d"
-      end
-  end
+  # apt-get not apt. apt will warn about non-stable cli interface.
+  DEBIAN_FRONTEND=noninteractive apt-get update 
+  apt-get -y upgrade >> /home/thor/.cron/logs/apt.log 2>&1
 end
 
-clean_home &>> $LOGFILE
+tk-apt-update &>> $LOGFILE
+
+
