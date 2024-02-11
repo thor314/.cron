e598f511c11e04111ee55deb7a0d9544e7ecfbcf
Thor Kampefner <thorck@pm.me>
2024-02-11 09:40:03 -0800

    "crow-2024-02-11-17:40UTC"

--- help_scripts/rotate_logs.fish
+++ help_scripts/rotate_logs.fish
@@ -4,29 +4,31 @@
 set LOGFILE $argv[1]
 set ONE_MB 1048576 # bytes
 
-
-function rotate_log_inspect
+function rotate_logs_inspect
   # If $LOGFILE is over 1mb in size, bump it back a number, and bump everything else back too
   if test -f $LOGFILE
     set size (wc -c $LOGFILE | string split " ")[1]
-    if test size -ge $ONE_MB
+    if test $size -ge $ONE_MB
       # do this first so that we log to $LOGFILE, not $LOGFILE.1
-      mv $LOGFILE $LOGFILE.tmp
-      echo "INFO: Rotating Logs. Moved $LOGFILE to $LOGFILE.1"
+      echo "INFO: $LOGFILE has size $size."
       rotate_logs
-      mv $LOGFILE.tmp $LOGFILE.1
+      mv $LOGFILE $LOGFILE.1
+      echo "INFO: Rotating Logs. Moved $LOGFILE to $LOGFILE.1"
     end
+  else 
+    echo "INFO: rotate_logs, nothing to do"
   end
 end
 
 function rotate_logs
   for i in (seq 8 -1 1) # 8 7 .. 1
     if test -f $LOGFILE.$i
-      echo "moving $LOGFILE.$i to $LOGFILE.$i_"
       set i_ (math "$i+1")
+      echo "moving $LOGFILE.$i to $LOGFILE.$i_"
+      # implicitly, this will overwrite $LOGFILE.9 if it is time to do so
       mv $LOGFILE.$i $LOGFILE.$i_
     end
   end
 end
 
-rotate_logs_inpspect &>> $LOGFILE
+rotate_logs_inspect &>> $LOGFILE
