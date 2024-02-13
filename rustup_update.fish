762d8682be04c231073c3a4587c12ef3f832ab99
Thor Kampefner <thorck@pm.me>
2024-02-13 14:18:32 -0800

    update cargo binaries

--- rustup_update.fish
+++ rustup_update.fish
@@ -6,6 +6,13 @@ set LOGFILE ~/.cron/logs/rustup_update.log
 function tk-rustup-update
   fish ~/.cron/help_scripts/cron_init.fish $LOGFILE
   /home/thor/.cargo/bin/rustup update 
+
+  # update all binaries
+  if not command -q cargo-update 
+    echo "INFO: install cargo update"
+    cargo install cargo-install-update
+  end
+  cargo install-update -a # update all binary packages
 end
 
 tk-rustup-update &>> $LOGFILE
