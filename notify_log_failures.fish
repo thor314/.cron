3d206db3d6a63c325bb3fafa66a9b2414817cb47
Thor Kampefner <thorck@pm.me>
2024-02-19 10:40:03 -0800

    "crow-2024-02-19-18:40UTC"

--- notify_log_failures.fish
+++ notify_log_failures.fish
@@ -6,10 +6,12 @@ set LOGFILE /home/thor/.cron/logs/notify_log_failures.log
 
 function notify-log-failures
   fish /home/thor/.cron/help_scripts/cron_init.fish $LOGFILE
-  # apt-get not apt. apt will warn about non-stable cli interface.
-  DEBIAN_FRONTEND=noninteractive apt-get update 
-  apt-get -y upgrade 
-  apt-get autoremove
+
+  
+end
+
+function filenames-from-crontab -d "extract the non-sudo filenames from the crontab"
+
 end
 
 tk-apt-update &>> $LOGFILE
