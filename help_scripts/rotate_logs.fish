e5e29680a0e996ac6474b80b7819903daf15f2a5
Thor Kampefner <thorck@pm.me>
2024-02-19 10:04:56 -0800

    rotate-logs minor refactor

--- help_scripts/rotate_logs.fish
+++ help_scripts/rotate_logs.fish
@@ -7,40 +7,24 @@
 set LOGFILE $argv[1]
 set MAX_SIZE 100000 # about 100kb, keep up to 1MB in history for each log in total
 
-function rotate -d "rotate the logs"
-  if test -f $LOGFILE # 
-    cat $LOGFILE >> $LOGFILE.1 && rm $LOGFILE
-  end
-
-end
-
-
-function rotate_logs_inspect
-  # If $LOGFILE is over $MAX_SIZE, bump it back a number, and bump everything else back too
-  if test -f $LOGFILE
-    set size (wc -c $LOGFILE | string split " ")[1]
-    if test $size -ge $MAX_SIZE
-      # do this first so that we log to $LOGFILE, not $LOGFILE.1
-      echo "INFO: $LOGFILE has size $size."
-      rotate_logs
-      mv $LOGFILE $LOGFILE.1
-      echo "INFO: Rotating Logs. Moved $LOGFILE to $LOGFILE.1"
-    end
-  else 
-    echo "INFO: rotate_logs, nothing to do"
-  end
-end
-
-function rotate_logs
-  for i in (seq 8 -1 1) # 8 7 .. 1
-    if test -f $LOGFILE.$i
-      set i_ (math "$i+1")
-      echo "moving $LOGFILE.$i to $LOGFILE.$i_"
-      # implicitly, this will overwrite $LOGFILE.9 if it is time to do so
-      mv $LOGFILE.$i $LOGFILE.$i_
+function rotate_logs -d "rotate the logs"
+  set size_first_archive (wc -c $LOGFILE.1 | string split " ")[1]
+  if test $size_first_archive_1 -ge $MAX_SIZE
+    echo "INFO: rotating log history"
+    for i in (seq 8 -1 1) # 8 7 .. 1
+      if test -f $LOGFILE.$i
+        set i_ (math "$i+1")
+        echo "moving $LOGFILE.$i to $LOGFILE.$i_"
+        # implicitly overwrite $LOGFILE.9 if exists
+        mv $LOGFILE.$i $LOGFILE.$i_
+      end
     end
   end
 end
 
-rotate_logs_inspect &>> $LOGFILE
-# rotate &>> $LOGFILE
+# first rotate the base log
+if test -f $LOGFILE 
+  cat $LOGFILE >> $LOGFILE.1 && rm $LOGFILE
+end
+# then rotate history, if applicable
+rotate_logs &>> $LOGFILE
