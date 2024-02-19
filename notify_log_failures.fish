e354b05a43d133e3d4a42346ed7903297746190b
Thor Kampefner <thorck@pm.me>
2024-02-19 11:00:03 -0800

    "crow-2024-02-19-19:00UTC"

--- notify_log_failures.fish
+++ notify_log_failures.fish
@@ -6,19 +6,15 @@ set LOGFILE /home/thor/.cron/logs/notify_log_failures.log
 
 function notify-log-failures
   fish /home/thor/.cron/help_scripts/cron_init.fish $LOGFILE
-  
-
-  
-end
-
-function filenames-from-crontab -d "extract the non-sudo filenames from the crontab"
-  cat /home/thor/.cron/crontab | # get the filenames
-  sed '/# sudo crontab:/,$d'   | # remove the sudo section
-  string match -vr "^#"        | # remove the comments
-  string replace -r '.* /' ''  | # get the filenames
-  string collect                 # remove trailing newline
+  set logfiles (ls /home/thor/.cron/logs | rg ".*\.log\$")
+  for f in $logfiles 
+    echo -e "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "INFO: searching logfile $f for issues"
+    echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+    # search f for instances of warning or error strings
+    rg -C2 "(W:|fatal|WARNING|ERROR|bad|exit|terminate|E:)"  $f
+  echo $f; end
 end
 
 tk-apt-update &>> $LOGFILE
-
 
